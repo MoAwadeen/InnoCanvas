5575e8f2b51095490e0af01ab5002e762343442a
Mohamed Awadeen <modyyahia44@gmail.com>
2025-07-27 19:54:48 +0000

    liqued qradient smooth animation

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -44,18 +44,6 @@ export interface ButtonProps
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, children, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
-    
-    if (variant === 'gradient') {
-      return (
-         <Comp
-          className={cn(buttonVariants({ variant, size, className }))}
-          ref={ref}
-          {...props}
-        >
-          <span className="btn-glow-content">{children}</span>
-        </Comp>
-      )
-    }
 
     return (
       <Comp
