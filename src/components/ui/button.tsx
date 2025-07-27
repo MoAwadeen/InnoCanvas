c4d33e6380d4656e0978ec1d10e5517483e527db
Mohamed Awadeen <modyyahia44@gmail.com>
2025-07-27 19:43:31 +0000

    import React from 'react'; import styled from 'styled-components';
    // B

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,7 +19,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-secondary hover:text-secondary-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-        gradient: "btn-realism",
+        gradient: "btn-gradient",
       },
       size: {
         default: "h-10 px-4 py-2",
@@ -52,7 +52,8 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
           ref={ref}
           {...props}
         >
-          <div className="blob" />
+          <div className="layer left" />
+          <div className="layer right" />
           <div className="inner">{children}</div>
         </Comp>
       )
