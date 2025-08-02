0e886f736b2029e901565429e1bb6a9c3dad3177
Mohamed Awadeen <93440998+MoAwadeen@users.noreply.github.com>
2025-08-02 18:18:48 +0300

    Analysis

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 import { AuthProvider } from '@/hooks/useAuth';
+import { Analytics } from "@vercel/analytics/next";
 
 export const dynamic = "force-dynamic";
 
@@ -28,6 +29,7 @@ export default function RootLayout({
             {children}
           </AuthProvider>
           <Toaster />
+          <Analytics />
       </body>
     </html>
   );
